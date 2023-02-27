fb8b52a66901d7b0a390d5f6cec0c906337bc57a
wuxh <wxh1220@gmail.com>
2023-02-27 09:40:46 +0800

    feat: beautiful print

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mri = require('mri');
 const open = require('open');
 const chalk = require('chalk');
+const utils = require('@wuxh/utils')
 
 const resolvePath = (...arg) => path.resolve(__dirname, ...arg);
 const readFileSync = (path) => fs.readFileSync(resolvePath(path), 'utf8');
@@ -31,6 +32,24 @@ const argv = mri(process.argv.slice(2), {
   default: { pick: Object.keys(contactList), speed: Math.floor(1000 / 60) }
 });
 
+function genLine(style = '-', color, length = 57) {
+  let line = style.repeat(length)
+  if (color) {
+    line = chalk.hex(color)(line)
+  }
+  return ['\n', line, '\n']
+}
+
+function genExample() {
+  const optionArr = utils.genCombinations(['o', 'h', 'p'], { mustInclude: ['p'] });
+  const pickClassify = Object.keys(contactList)
+
+  const randomOption = optionArr[utils.randomIntegerInRange(0, optionArr.length - 1)];
+  const randomPick = pickClassify[utils.randomIntegerInRange(0, pickClassify.length - 1)];
+
+  return `${package.name} -${randomOption} ${randomPick}`
+}
+
 async function main(args = argv) {
 
   const pickLinks = (
@@ -44,29 +63,29 @@ async function main(args = argv) {
   const hideLogo = args.hidelogo;
 
   if (args.version) {
-    console.log(`${chalk.bold(package.name)}: ${chalk.blue('v' + package.version)}`);
+    console.log(`${chalk.bold(package.name)}: ${chalk.green('v' + package.version)}`);
     return;
   }
 
   if (args.help || Object.keys(links).length === 0) {
     console.log(`
-    ${chalk.green(`${package.name} [options]`)}
-      -p, --pick [${Object.keys(contactList).join('|')}]
-      -o, --open: use default browser to open the link.
-      -h, --hidelogo: hide auther logo.
-      -s, --speed: set the speed of animation.
-      -H, --help: show help.
-      -v, --version: show version. ${chalk.blue('v' + package.version)}
+    ${chalk.green(`${chalk.bold(package.name)} [options]`)}
+      -${chalk.bold('p')}, --pick [${Object.keys(contactList).map(val=>chalk.hex('#91caff')(val)).join('|')}]
+      -${chalk.bold('o')}, --open: use default browser to open the link.
+      -${chalk.bold('h')}, --hidelogo: hide auther logo.
+      -${chalk.bold('s')}, --speed: set the speed of animation.
+      -${chalk.bold('H')}, --help: show help.
+      -${chalk.bold('v')}, --version: show version. ${chalk.green('v' + package.version)}
       ----------------------------------------
-      ${chalk.bold('e.g.')} ${chalk.green(`${package.name} -hop ${Object.keys(contactList)[0]}`)}
+      ${chalk.bold('e.g.')} ${chalk.green(genExample())}
     `)
     return;
   }
 
   if (!hideLogo) {
-    console.log(autherLogo);
+    console.log(chalk.hex(utils.randomHexColorCode())(autherLogo));
     await sleep(args.speed)
-    console.log('\n', '='.repeat(57), '\n');
+    console.log(...genLine('=', '#91caff'));
   } else {
     console.log(chalk.bold(`${chalk.green('Author')}: ${package.author.name}<${package.author.email}>`));
   }
@@ -77,11 +96,10 @@ async function main(args = argv) {
     await sleep(args.speed);
     if (args.open) await open(link);
     const _name = name.charAt(0).toUpperCase() + name.slice(1);
-    console.log(chalk.blue(`  ${chalk.bold(_name)}:`), chalk.underline(chalk.blue(link)));
+    console.log(chalk.hex('#1677ff')(`  ${chalk.bold(_name)}:`), chalk.underline(chalk.hex('#eb2f96')(link)));
   }
 
-  !hideLogo && console.log('\n', '='.repeat(57), '\n');
-
+  !hideLogo && console.log(...genLine('=', '#91caff'));;
 }
 
 module.exports = main;
